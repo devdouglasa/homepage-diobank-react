92337c6510905abe2c94ed2cd45163ce1f7d0134
devdouglasa <douglasalisson27@gmail.com>
2024-11-29 22:10:31 -0300

    refactor: refatored component Main

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import { Card } from "./Layout/Card"
+import { Footer } from "./Footer/Footer"
+import { Header } from "./Header/Header"
+
+
+export const Layout = () => {
+  return(
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  )
+}
