c0d4ee78e808719c105a1596bfb565e1c24c262d
devdouglasa <douglasalisson27@gmail.com>
2024-11-29 22:08:39 -0300

    feat: add component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css'
 export const Header  = () => {
   return(
     <div className='header'>
-      Dio Bank
+      <strong><h3 className='header-title'>Dio Bank</h3></strong>
     </div>
   )
 }
