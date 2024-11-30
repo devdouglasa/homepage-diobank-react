df94423228b5497e38dbbfafdf6d5fb562e14479
devdouglasa <douglasalisson27@gmail.com>
2024-11-29 22:06:24 -0300

    feat: upgrade component Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export const Footer  = () => {
+  return(
+    <div className='container-footer'>
+      <strong><p className='container-footer-text'>Desenvolvido por Dev Douglas | 2024</p></strong>
+    </div>
+  )
+}
