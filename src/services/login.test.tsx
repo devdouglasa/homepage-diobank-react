51fd23c2153b82d6bfb15729e3d0c7459db94a14
devdouglasa <douglasalisson27@gmail.com>
2024-11-29 22:18:07 -0300

    refactor: refatored message it

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -5,7 +5,7 @@ describe('login', () => {
     const mockAlert = jest.fn()
     window.alert = mockAlert
 
-    it('Deve exibir um alert com boas vindas', () => {
+    it('Exibe um alert com boas vindas', () => {
         login()
         expect(mockAlert).toHaveBeenCalledWith('Seja Bem vindo Douglas!')
     })
